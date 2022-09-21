d10ec6db62cdb10fddd95d73cdfd3fd2ce8a735c
Regita-99 <regita.roseprameswari2017@students.unila.ac.id>
2022-09-21 12:53:34 +0700

    Update Mahasiswa.php

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -9,11 +9,11 @@ class Mahasiswa extends Model
     protected $table            = 'mahasiswa';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields    = ['npm', 'nama', 'alamat', 'created_at', 'updated_at'];
+    protected $allowedFields    = ['npm', 'nama', 'alamat', 'created_at', 'update_at'];
 
     // Dates
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
+    protected $updatedField  = 'update_at';
 }
