69d676ff354abb2d60083907cfa3e0967130a1e0
Regita-99 <regita.roseprameswari2017@students.unila.ac.id>
2022-10-11 05:57:53 +0700

    ubah dikit

--- app/Views/mahasiswa/create.php
+++ app/Views/mahasiswa/create.php
@@ -1,36 +1,36 @@
 <?= $this->extend('templates/template'); ?>
 <?= $this->section('content'); ?>
 <div class="row">
-<div class="col-6">
-<form action="/store" method="post">
+    <div class="col-6">
+        <form action="/store" method="post">
 
-        <div class="mb-3">
-            <label for="npm" class="form-label">NPM</label>
-            <input type="text" name="npm" class="form-control" id="npm">
-        </div>
-        <div class="mb-3">
-            <label for="nama" class="form-label">Nama</label>
-            <input type="text" name="nama" class="form-control" id="nama">
-        </div>
-        <div class="mb-3">
-            <label for="alamat" class="form-label">Alamat</label>
-            <input type="text" name="alamat" class="form-control" id="alamat">
-        </div>
+            <div class="mb-3">
+                <label for="npm" class="form-label">NPM</label>
+                <input type="text" name="npm" class="form-control" id="npm">
+            </div>
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" name="nama" class="form-control" id="nama">
+            </div>
+            <div class="mb-3">
+                <label for="alamat" class="form-label">Alamat</label>
+                <input type="text" name="alamat" class="form-control" id="alamat">
+            </div>
     </div>
     <div class="col-6">
-    <div class="mb-3">
+        <div class="mb-3">
             <label for="deskripsi" class="form-label">deskripsi</label>
             <textarea name="deskripsi" class="form-control" id="deskripsi"></textarea>
         </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    
-    </form>
     </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
 </div>
 
+
 <div class="p-4">
-    
-       
+
+
 </div>
 <?= $this->endSection('content'); ?>
